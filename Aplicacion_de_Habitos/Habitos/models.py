3f43317d3d5740ded100d06d5997a1e8f29bd564
Vania <sonalysifuentes@gmail.com>
2025-04-09 14:26:12 -0500

    Crear formulario HabitForm para creación de hábitos

--- Aplicacion_de_Habitos/Habitos/models.py
+++ Aplicacion_de_Habitos/Habitos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Habit(models.Model):
+class Habito(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
